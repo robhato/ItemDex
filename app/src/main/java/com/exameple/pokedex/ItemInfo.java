3fb6ea0d17cb0565950b80f2ba568f4f44bf4671
Jaideep <jaideep.bhat32@gmail.com>
2019-05-01 01:55:59 -0500

    Working on item info page

---
+++ app/src/main/java/com/exameple/pokedex/ItemInfo.java
@@ -0,0 +1,49 @@
+package com.exameple.pokedex;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.exameple.pokedex.api.pokeapi;
+import com.exameple.pokedex.pokemon.PokemonList;
+import com.exameple.pokedex.pokemon.Pokemon;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ItemInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_info);
+        Button backButton = (Button) findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(ItemInfo.this, MainActivity.class));
+            }
+        });
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String name = extras.getString("name");
+        String img = extras.getString("image");
+        textView.setText(name);
+        //textView2.setText(img);
+    }
+}
